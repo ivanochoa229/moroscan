8124eefd37b93c528ced8a61a8aab7bc4ac49123
ivanochoa229 <ivanochoa229@gmail.com>
2025-06-07 09:50:43 -0300

    front

--- MoroScan/vite.config.js
+++ MoroScan/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: '/', // Asegura que los assets se resuelvan correctamente
+  ...(mode === 'development' && {
+    server: {
+      proxy: {
+        '/production': 'http://localhost:8080',
+      },
+    },
+  }),
+}))
