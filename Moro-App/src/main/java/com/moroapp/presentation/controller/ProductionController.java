960bd8e69f5852dfda23e8c995e12fd568e68c19
ivanochoa229 <ivanochoa229@gmail.com>
2025-06-07 12:58:56 -0300

    cambios

--- Moro-App/src/main/java/com/moroapp/presentation/controller/ProductionController.java
+++ Moro-App/src/main/java/com/moroapp/presentation/controller/ProductionController.java
@@ -10,11 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/production")
-@CrossOrigin(origins = {
-    "http://18.216.227.102",
-    "http://18.216.227.102:80",
-    "http://localhost:80"
-})
+@CrossOrigin(origins = {"http://localhost:3000", "http://18.216.227.102", "http://18.216.227.102:3000"})
 public class ProductionController {
 
     private final ProductionService productionService;
